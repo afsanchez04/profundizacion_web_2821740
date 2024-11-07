cf9f86b1ac883d39c8b60fc314dd60395a50e0ea
afsaanchez04 <afsanchez04@gmail.com>
2024-11-07 08:17:00 -0500

    dom avanzado

--- 2-DOM-avanzado/js/app.js
+++ 2-DOM-avanzado/js/app.js
@@ -1,1 +1,31 @@
-console.log("hola mundo")
+
+const container = document.querySelector(".container")
+
+capitulos.forEach( function (cap){
+
+    const card = document.createElement("button")
+    card.classList.add("card")
+    card.style.backgroundImage = `url(${cap.portada})`
+    card.style.backgroundSize= "cover"
+    card.style.backgroundPosition= "center"
+
+    if(cap.habilitado){
+        card.classList.remove("disabled")
+    }else{
+        card.classList.add("disabled")
+        card.disabled = true
+    }
+
+    card.addEventListener( "click", function () {
+        console.log( cap )
+        localStorage.setItem("currentCapitulo", JSON.stringify(cap) )
+        window.location.href = "../pages/comic.html"
+    } )
+
+    card.innerHTML = `
+            <h1>${cap.cap}</h1>
+    `
+
+    container.appendChild(card)
+
+} )
---
+++ 2-DOM-avanzado/js/capitulosbd.js
@@ -0,0 +1,22 @@
+//array de objetos
+
+let capitulos = [
+    {
+        cap: 1,
+        habilitado: true,
+        currentScene: 1,
+        portada: "../assets/images/bgroom.jpg"
+    },
+    {
+        cap: 2,
+        habilitado: false,
+        currentScene: 1,
+        portada: "../assets/images/grog.jpg"
+    },
+    {
+        cap: 3,
+        habilitado: true,
+        currentScene: 1,
+        portada: "../assets/images/images.jpg"
+    }
+]
